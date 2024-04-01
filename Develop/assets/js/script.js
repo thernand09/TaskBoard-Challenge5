8c377ddad96190798444da9f0f00a6e829a6ab36
Thannia Hernandez <thaherna1@gmail.com>
2024-03-31 19:39:26 -0500

    Added Modal from bootstrap page

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -2,9 +2,11 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
+
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+localStorage.generateTaskId('nextID', JSON.stringify(nextID));
+return nextId++;
 }
 
 // Todo: create a function to create a task card
@@ -34,5 +36,7 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+renderTaskList();
+
 
 });
